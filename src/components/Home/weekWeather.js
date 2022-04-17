4029b9db4434b5952ff77f0472142f630b005b8b
Taher <ttaherhagui@gmail.com>
2022-04-17 16:36:29 +0100

    style weekWeather.

--- src/components/Home/weekWeather.js
+++ src/components/Home/weekWeather.js
@@ -2,19 +2,20 @@ import { React } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import './weekWeather.css';
 import SUNNY from '../../Statics/Images/sun.svg';
 
 const WeakWEather = () => {
   let index = 0;
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 6, sm: 12, md: 12 }} className="container">
         {Array.from(Array(6)).map(() => {
           index += 1;
           return (
-            <Grid item xs={2} sm={4} md={4} key={index}>
-              <Typography variant="h2"> MON </Typography>
-              <img src={SUNNY} alt="weather-icon" />
+            <Grid item xs={2} sm={4} md={4} key={index} className="weekday-weather">
+              <Typography variant="h5" className="day-name"> MON </Typography>
+              <img src={SUNNY} alt="weather-icon" className="weather-img" />
               <Typography variant="h6" style={{ color: '#fff', textAlign: 'center' }}>
                 15°c
               </Typography>
