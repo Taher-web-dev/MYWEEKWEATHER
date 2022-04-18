54c01697cdfdf3a07829b668c6dfd6a01c5b0d8b
Taher <ttaherhagui@gmail.com>
2022-04-18 17:36:13 +0100

    Add snapshot test for weekWeather component.

---
+++ src/__test__/weekWeather.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import WeakWEather from '../components/Home/weekWeather';
+
+const mockStore = configureStore([]);
+describe('render correctly weekWeather component', ()=> {
+  let store;
+  let tree;
+  beforeEach(() => {
+    store = mockStore({
+      city: {},
+      weather: {},
+      toggle: true,
+    });
+    store.dispatch = jest.fn();
+    tree = render(
+      <Provider store={store}>
+        <WeakWEather />
+      </Provider>
+    );
+  });
+  it('it should render with given state from redux store', () => {
+    expect(tree).toMatchSnapshot();
+  });
+});
