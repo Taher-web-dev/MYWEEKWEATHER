f2c551617a24d6eaffbc4f91e1bca638dba8547e
Taher <ttaherhagui@gmail.com>
2022-04-18 22:07:43 +0100

    add snapshot test for home component.

--- src/__test__/home.test.js
+++ src/__test__/home.test.js
@@ -3,8 +3,10 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import Home from '../components/Home/home';
+import XMLHttpRequest from 'xhr2';
 
 const mockStore = configureStore([]);
+const mockGeolocation = {  getCurrentPosition: jest.fn(),  watchPosition: jest.fn()};global.navigator.geolocation = mockGeolocation;
 describe('render correctly weekWeather component', () => {
   let store;
   let tree;
@@ -21,7 +23,7 @@ describe('render correctly weekWeather component', () => {
       </Provider>,
     );
   });
-  it('it should render with given state from redux store', () => {
+  it('it should render home component with given state from redux store', () => {
     expect(tree).toMatchSnapshot();
   });
 });
